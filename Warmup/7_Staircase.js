8e25aed20d164b6306988cdeb1a6a7b4991e0874
shahariartushar <shahariartushar@gmail.com>
2022-02-27 22:18:54 +0100

    complete staircase

---
+++ Warmup/7_Staircase.js
@@ -0,0 +1,49 @@
+//https://www.hackerrank.com/challenges/staircase/problem?isFullScreen=true
+
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+function staircase(n) {
+  // Write your code here
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n - i; j++) {
+      process.stdout.write(" ");
+    }
+    for (let k = 1; k <= i; k++) {
+      process.stdout.write("#");
+    }
+
+    console.log();
+  }
+}
+
+function main() {
+  const n = parseInt(readLine().trim(), 10);
+
+  staircase(n);
+}
