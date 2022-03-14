a67872753757b6d301e608f19ec8cb6fca465b77
shahariartushar <shahariartushar@gmail.com>
2022-03-14 15:19:53 +0100

    complete drawing book

---
+++ Implementation/12_Drawing_Book.js
@@ -0,0 +1,60 @@
+//https://www.hackerrank.com/challenges/drawing-book/problem?isFullScreen=true
+
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+function pageCount(n, p) {
+  // Write your code here
+
+  let pageTurns = Math.floor(p / 2);
+  let totalTurns = Math.floor(n / 2);
+
+  /* Returns the total number of page turns it takes to get
+    to a page or how many it requires if starting from the back */
+
+  return Math.min(pageTurns, totalTurns - pageTurns);
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const p = parseInt(readLine().trim(), 10);
+
+  const result = pageCount(n, p);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
