49aff100edb63f6f3a58c499301798b569e6325c
shahariartushar <shahariartushar@gmail.com>
2022-03-24 21:03:43 +0100

    complete angr professor

--- Implementation/22_Angry_Professor.js
+++ Implementation/22_Angry_Professor.js
@@ -1,27 +1,27 @@
 //https://www.hackerrank.com/challenges/angry-professor/problem?isFullScreen=true
 
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -34,36 +34,39 @@ function readLine() {
  */
 
 function angryProfessor(k, a) {
-    // Write your code here
-    let count=0;
-    
-    for(let i=0;i<a.length;i++){
-        if(a[i]<=0){
-            count += 1;
-        }
+  // Write your code here
+  let count = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] <= 0) {
+      count += 1;
     }
-    
-    return (count>=k)?'NO':'YES';
+  }
+
+  return count >= k ? "NO" : "YES";
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const t = parseInt(readLine().trim(), 10);
+  const t = parseInt(readLine().trim(), 10);
 
-    for (let tItr = 0; tItr < t; tItr++) {
-        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+  for (let tItr = 0; tItr < t; tItr++) {
+    const firstMultipleInput = readLine().replace(/\s+$/g, "").split(" ");
 
-        const n = parseInt(firstMultipleInput[0], 10);
+    const n = parseInt(firstMultipleInput[0], 10);
 
-        const k = parseInt(firstMultipleInput[1], 10);
+    const k = parseInt(firstMultipleInput[1], 10);
 
-        const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+    const a = readLine()
+      .replace(/\s+$/g, "")
+      .split(" ")
+      .map((aTemp) => parseInt(aTemp, 10));
 
-        const result = angryProfessor(k, a);
+    const result = angryProfessor(k, a);
 
-        ws.write(result + '\n');
-    }
+    ws.write(result + "\n");
+  }
 
-    ws.end();
+  ws.end();
 }
