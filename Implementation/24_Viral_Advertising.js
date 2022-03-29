1f838e4f2a49df392d8d9971cd6e5c807fb56778
shahariartushar <shahariartushar@gmail.com>
2022-03-29 16:04:18 +0200

    complete viral advertising

---
+++ Implementation/24_Viral_Advertising.js
@@ -0,0 +1,57 @@
+//https://www.hackerrank.com/challenges/strange-advertising/problem?isFullScreen=true
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'viralAdvertising' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function viralAdvertising(n) {
+    // Write your code here
+    let received = 5;
+    let cumulative = 0;
+    for(let i = 0; i < n; i++) {
+        received = Math.floor(received/2);
+        cumulative += received;
+        received *= 3;
+    }
+    
+    return cumulative;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const result = viralAdvertising(n);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
